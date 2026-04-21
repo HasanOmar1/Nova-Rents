68633c313fb37699274c3a4d68f5debf323d8a0a
Hasan Omar <r3domar1@gmail.com>
2026-04-21 15:42:11 +0300

    added header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,14 +1,56 @@
 import styles from "./Header.module.css";
+import { Car, Menu, Bell, LogOut } from "lucide-react";
+import { navByRole, labels, icons } from "./nav";
 
-const Header = () => {
+const Header = ({ role, page }) => {
+  const visible = navByRole[role] || [];
+
   return (
     <header className={styles.Header}>
       <div className={styles.siteDetails}>
-        <div className={styles.logo}>{/* <img src="" alt="" /> */}</div>
+        <div className={styles.logoAndName}>
+          <div className={styles.logo}>
+            <Car size={32} />
+          </div>
+          <div className={styles.siteName}>
+            <h2>Nova Rents</h2>
+            <p>Luxury vehicle rental</p>
+          </div>
+        </div>
 
-        <div className={styles.siteName}>
-          <h2>Nova Rents</h2>
-          <p>Luxury vehicle rental</p>
+        <nav>
+          {visible.map((item) => {
+            const Icon = icons[item];
+            const active = page === item;
+
+            return (
+              <button
+                key={item}
+                // onClick={() => setPage(item)}
+                className={`${styles.navButton}  ${active ? styles.active : ""}`}
+              >
+                {Icon && <Icon className="nav-icon" />}
+                {labels[item]}
+              </button>
+            );
+          })}
+        </nav>
+
+        <div className={styles.logOutContainer}>
+          <>
+            <button className={styles.notifyButton}>
+              <Bell className={styles.iconLarge} />
+            </button>
+
+            <button className={styles.logoutButton}>
+              <LogOut className={styles.iconSmall} />
+              Log out
+            </button>
+          </>
+
+          {/* <button type="button" className={styles.menuButton}>
+            <Menu className={styles.iconLarge} />
+          </button> */}
         </div>
       </div>
     </header>
