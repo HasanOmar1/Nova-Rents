1b237c06544d1aec27f2e3b91ae3e1768ac7dde1
Hasan Omar <r3domar1@gmail.com>
2026-04-22 13:46:25 +0300

    updated auth middleware

--- backend/middleWare/authMiddleware.js
+++ backend/middleWare/authMiddleware.js
@@ -1,11 +1,15 @@
 // Middleware to ensure the user is logged in before accessing protected routes
+const STATUS_CODE = require("../constants/statusCodes");
+
 const isAuthenticated = (req, res, next) => {
   console.log(req.session);
   if (req.session.user) {
     return next();
   } else {
     console.error("User not logged in");
-    return res.status(401).json({ message: "Login to do this action!" });
+    return res
+      .status(STATUS_CODE.UNAUTHORIZED)
+      .json({ message: "Login to do this action!" });
   }
 };
 
@@ -15,7 +19,9 @@ const isAdmin = (req, res, next) => {
     return next();
   } else {
     console.error("User not logged in");
-    return res.status(401).json({ message: "Login to do this action!" });
+    return res
+      .status(STATUS_CODE.UNAUTHORIZED)
+      .json({ message: "You must be an admin to do this action!" });
   }
 };
 module.exports = { isAuthenticated, isAdmin };
