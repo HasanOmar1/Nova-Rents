f93620133e804f1ec183b8a882e0c84bfc9dd813
Hasan Omar <r3domar1@gmail.com>
2026-04-28 21:11:14 +0300

    Vehicles page: started with filter

--- frontend/src/pages/Vehicles/Vehicles.jsx
+++ frontend/src/pages/Vehicles/Vehicles.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import styles from "./Vehicles.module.css";
+import { Search } from "lucide-react";
 
 const tabs = ["all", "cars", "motorcycles", "eventVehicles"];
 
 const Vehicles = () => {
   const [activeTab, setActiveTab] = useState("all");
+  const [locations, setLocations] = useState([
+    "All Locations",
+    "Haifa",
+    "Tel Aviv",
+    "Nazareth",
+    "Jerusalem",
+  ]);
+  const [sortBy, setSortBy] = useState(["price", "year"]);
+
   return (
     <div className={`${styles.Vehicles} page`}>
       <h1>Vehicles</h1>
@@ -29,22 +39,27 @@ const Vehicles = () => {
 
         <hr />
 
-        <div className="inputsContainer">
-          <div className="top">
-            <input
-              type="text"
-              placeholder="Search name or location"
-              className="searchNameOrLocation"
-            />
+        <div className={styles.inputsContainer}>
+          <div className={styles.top}>
+            <div className={styles.searchContainer}>
+              <Search size={20} color="gray" />
+              <input
+                type="text"
+                placeholder="Search name or location"
+                className={styles.searchNameOrLocationInput}
+              />
+            </div>
 
             <select name="location" id="location">
-              <option value="all">All Locations</option>
-              {/* add all locations */}
+              {locations.map((location) => {
+                return <option value={location}>{location}</option>;
+              })}
             </select>
 
             <select name="sort" id="sort">
-              <option value="price">Sort: price</option>
-              {/* add sort by what... */}
+              {sortBy.map((sort) => {
+                return <option value={sort}>Sort: {sort}</option>;
+              })}
             </select>
           </div>
 
