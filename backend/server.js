499675f2f4fb3b4e925b3bdb4233875571f87a56
Hasan Omar <r3domar1@gmail.com>
2026-05-23 12:03:45 +0300

    updated package.json + added dotenv package+

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const cors = require("cors");
 const errorHandler = require("./middleWare/errorMiddleware");
+require("dotenv").config();
 
 // routes
 const usersRoute = require("./routes/usersRoute");
