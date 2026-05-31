7a99db328cf98f436f84bd2aa85092eced85296a
Essa Lwabne <193935852+Essalwabne@users.noreply.github.com>
2026-05-31 14:13:38 +0300

    nodemailer

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -5,11 +5,13 @@ const { isAuthenticated, isAdmin } = require("../middleWare/authMiddleware");
 const userQueries = require("../controllers/usersController");
 
 router.get("/", userQueries.getAllUsers);
-router.get("/details", userQueries.getUserDetailsByEmail);
+router.get("/details", isAuthenticated, userQueries.getUserDetailsByEmail);
 router.post("/register", userQueries.register);
 router.post("/login", userQueries.login);
 router.get("/profile", isAuthenticated, userQueries.getProfile);
 router.post("/logout", isAuthenticated, userQueries.logout);
 router.post("/block", isAdmin, userQueries.blockUserByEmail);
 router.post("/unblock", isAdmin, userQueries.unblockUserByEmail);
+router.put("/profile", isAuthenticated, userQueries.updateUserProfile);
+router.post("/verify-code", isAuthenticated, userQueries.verifyCode);
 module.exports = router;
