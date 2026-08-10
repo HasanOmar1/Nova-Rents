94adc715cbe42e1b428122e0a9ded41fafbd78f2
Hasan <r3domar1@gmail.com>
2026-08-10 11:51:56 +0300

    added remove warning action

--- backend/routes/reportedUsersRoute.js
+++ backend/routes/reportedUsersRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { isAdmin } = require("../middleWare/authMiddleware");
-const { listReportedUsers, listUserReports, listWarnings, warnUser } = require("../controllers/reportedUsersController");
+const { listReportedUsers, listUserReports, listWarnings, warnUser, removeLatestWarning } = require("../controllers/reportedUsersController");
 
 const router = express.Router();
 router.use(isAdmin);
@@ -8,4 +8,5 @@ router.get("/", listReportedUsers);
 router.get("/:userId/reports", listUserReports);
 router.get("/:userId/warnings", listWarnings);
 router.post("/:userId/warnings", warnUser);
+router.delete("/:userId/warnings/latest", removeLatestWarning);
 module.exports = router;
